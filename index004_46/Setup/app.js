162dafeaaba06723364c300c93dc7f6749cd5195
Misako Ohata <misa@ohatadesign.com>
2017-01-19 23:52:43 +0900

    adding index005

--- index004_46/Setup/app.js
+++ index004_46/Setup/app.js
@@ -3,18 +3,24 @@ new Vue({
   data: {
     playerHealth:100,
     monsterHealth:100,
-    gameIsRunning: false
-
+    gameIsRunning: false,
+    turns:[]
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
-      
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
+
       // ゲームを継続するかどうか聞く
       if (this.checkWin()){
           return;
@@ -23,7 +29,12 @@ new Vue({
 
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster  hard for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -36,15 +47,24 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       
       this.monsterAttacks();
     },
     giveUp: function() {
-      this.gameIsRunning 
+      this.gameIsRunning
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -55,7 +75,6 @@ new Vue({
           this.startGame();
         } else {
           this.gameIsRunning = false;
-          this.
         }
         return true;
       } else if (this.playerHealth <= 0) {
