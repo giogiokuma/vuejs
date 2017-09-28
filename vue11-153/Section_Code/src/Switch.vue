75e1279407e42d608b15480e0c8a09bc8fc311b4
Misako Ohata <ohata@raijin.com>
2017-09-28 11:06:27 +0900

    initialize Section_Code

---
+++ vue11-153/Section_Code/src/Switch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <div
+        id="on"
+        @click="value = true"
+        :class="{active: value}">On</div>
+      <div
+        id="off"
+        @click="value = false"
+        :class="{active: !value}">Off</div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    methods: {
+      switched() {
+        this.$emit('input');
+      }
+    }
+  }
+</script>
+<style scoped>
+  #on, #off {
+    width: 40px;
+    height: 20px;
+    background-color: lightgray; 
+    padding:2px;
+    display:inline-block; 
+    margin:10x -2px;
+    box-sizing:content-box;
+    cursor: pointer; 
+    text-align:center;
+  }
+
+  #on:hover, #on.active {
+    background-color: lightgreen;
+  }
+
+  #off:hover, #off.active {
+    background-color: lightcoral;
+  }
+</style>
